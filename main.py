71caa72a11648033039efc7858b09385892a8200
baletor <kubierj@gmail.com>
2025-07-01 09:46:13 -0600

    fixed minor error in system prompt

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ system_prompt = """
 You are a helpful AI coding agent.
 
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 - List files and directories
 - Read file contents
 - Execute Python files with optional arguments
